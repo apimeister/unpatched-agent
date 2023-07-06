7ba43fa9da324e7840a5559b057290435029d930
schoki040 <a.kunde@outlook.com>
2023-07-06 22:25:17 +0200

    use mpsc channel to enable data send via ping from server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use clap::Parser;
+use futures_util::stream::SplitSink;
 use futures_util::{SinkExt, StreamExt};
+use once_cell::sync::Lazy;
 use std::fs;
-use std::ops::ControlFlow;
+use std::ops::{ControlFlow, Deref};
+use std::sync::{Arc, Mutex};
 use std::time::Duration;
-use tokio_tungstenite::connect_async;
+use sysinfo::{System, SystemExt};
+use tokio::net::TcpStream;
+use tokio::sync::mpsc::{self, Receiver, Sender};
+use tokio::task::JoinHandle;
 use tokio_tungstenite::tungstenite::protocol::Message;
+use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
 use uuid::Uuid;
 
 #[derive(Parser, Debug)]
@@ -18,12 +25,70 @@ struct Args {
 
 const RETRY: Duration = Duration::new(5, 0); //set to desired number
 
+// static SYSINFO: Lazy<Mutex<System>> =
+//     Lazy::new(|| {let mut sys = System::new_all(); sys.refresh_all(); sys.into() });
+
 #[tokio::main]
 async fn main() {
     let agent_id = Uuid::new_v4().to_string();
     // Dont die on connection loss
+    let args = Args::parse();
+    let who = 0;
+
     loop {
-        let _ = tokio::spawn(spawn_client(0, agent_id.clone())).await;
+        // create new channel to trigger data send everytime we restart the client
+        // with mpsc we only every allow one receiver
+        let (tx, mut rx) = mpsc::channel::<bool>(8);
+
+        let id = agent_id.clone();
+        let alias = args.alias.clone();
+
+        // get websocket stream
+        let (ws_stream, _) = tokio_tungstenite::connect_async(&args.server)
+            .await
+            .unwrap();
+        // split websocket stream so we can have both directions working independently
+        let (mut sender, mut receiver) = ws_stream.split();
+
+        // trigger first data send via mpsc channel
+        let _tx_send = tx.send(true).await;
+
+        // all things outgoing
+        let sender_handle = tokio::spawn(async move {
+            let mut sink = sender;
+            let _ping = sink.feed(Message::Ping("Hello, Server!".into())).await;
+
+            loop {
+                if let Some(rec) = rx.recv().await {
+                    let mut sys = System::new_all();
+                    sys.refresh_all();
+                    let os_version = sys.long_os_version().unwrap_or("".into());
+                    let uptime = sys.uptime().to_string();
+
+                    let _send = sink.feed(Message::Text(format!("uuid:{id}"))).await;
+                    let _send = sink.feed(Message::Text(format!("alias:{alias}"))).await;
+                    let _send = sink.feed(Message::Text(format!("os:{os_version}"))).await;
+                    let _send = sink.feed(Message::Text(format!("uptime:{uptime}"))).await;
+                    let _flush = sink.flush().await;
+                }
+                // dont go crazy, sleep for a while after checking for data/sending data
+                tokio::time::sleep(RETRY).await;
+            }
+        });
+
+        // all things incoming
+        let recv_handle = tokio::spawn(async move {
+            while let Some(Ok(msg)) = receiver.next().await {
+                // print message and break if instructed to do so
+                if process_message(msg, who, tx.clone()).is_break() {
+                    println!("we are breaking!");
+                    break;
+                }
+            }
+        });
+
+        let _ = recv_handle.await;
+
         println!(
             "Lost Connection to server, retrying in {} seconds ...",
             RETRY.as_secs()
@@ -32,93 +97,9 @@ async fn main() {
     }
 }
 
-//creates a client. quietly exits on failure.
-async fn spawn_client(who: usize, agent_id: String) {
-    let args = Args::parse();
-    // let req = Request::builder()
-    //     .method("GET")
-    //     .header("Host", args.hostname)
-    //     .header("Connection", "Upgrade")
-    //     .header("Upgrade", "websocket")
-    //     .header("Sec-WebSocket-Version", "13")
-    //     .header("Sec-WebSocket-Key", generate_key())
-    //     .header("User-Agent", "internal-monitoring-agent/1.0")
-    //     .uri(args.server)
-    //     .body(())
-    //     .unwrap();
-
-    let ws_stream = match connect_async(args.server).await {
-        Ok((stream, response)) => {
-            println!("Handshake for client {} has been completed", who);
-            // This will be the HTTP response, same as with server this is the last moment we
-            // can still access HTTP stuff.
-            println!("Server response was {:?}", response);
-            stream
-        }
-        Err(e) => {
-            println!("WebSocket handshake for client {who} failed with {e}!");
-            return;
-        }
-    };
-
-    let (mut sender, mut receiver) = ws_stream.split();
-
-    //we can ping the server for start
-    sender
-        .send(Message::Ping("Hello, Server!".into()))
-        .await
-        .expect("Can not send!");
-
-    //spawn an async sender to push some more messages into the server
-    let send_task = tokio::spawn(async move {
-        let _send_alias = sender
-            .send(Message::Text("uuid:".to_string() + &agent_id))
-            .await;
-
-        let _send_alias = sender
-            .send(Message::Text("alias:".to_string() + &args.alias))
-            .await;
-        let _send_os_version = sender
-            .send(Message::Text("os:".to_string() + &read_os_version()))
-            .await;
-        let _send_uptime = sender
-            .send(Message::Text("uptime:".to_string() + &read_uptime()))
-            .await;
-    });
-
-    //receiver just prints whatever it gets
-    let recv_task = tokio::spawn(async move {
-        while let Some(Ok(msg)) = receiver.next().await {
-            // print message and break if instructed to do so
-            if process_message(msg, who).is_break() {
-                println!("we are breaking!");
-                break;
-            }
-        }
-    });
-
-    let a = send_task.await;
-    let b = recv_task.await;
-
-    println!("a: {:?}", a);
-    println!("b: {:?}", b);
-
-    // When we are done we may want our client to close connection cleanly.
-    // println!("Sending close to {}...", who);
-    // if let Err(e) = sender
-    //     .send(Message::Close(Some(CloseFrame {
-    //         code: CloseCode::Normal,
-    //         reason: Cow::from("Goodbye"),
-    //     })))
-    //     .await
-    // {
-    //     println!("Could not send Close due to {:?}, probably it is ok?", e);
-    // };
-}
-
 /// Function to handle messages we get (with a slight twist that Frame variant is visible
 /// since we are working with the underlying tungstenite library directly without axum here).
-fn process_message(msg: Message, who: usize) -> ControlFlow<(), ()> {
+fn process_message(msg: Message, who: usize, tx: Sender<bool>) -> ControlFlow<(), ()> {
     match msg {
         Message::Text(t) => {
             println!(">>> {} got str: {:?}", who, t);
@@ -146,6 +127,9 @@ fn process_message(msg: Message, who: usize) -> ControlFlow<(), ()> {
         // v according to spec. But if you need the contents of the pings you can see them here.
         Message::Ping(v) => {
             println!(">>> {} got ping with {:?}", who, v);
+            tokio::spawn(async move {
+                tx.send(true).await.unwrap();
+            });
         }
 
         Message::Frame(_) => {
@@ -153,25 +137,4 @@ fn process_message(msg: Message, who: usize) -> ControlFlow<(), ()> {
         }
     }
     ControlFlow::Continue(())
-}
-
-fn read_os_version() -> String {
-    println!("os");
-    match fs::read_to_string("/etc/os-release") {
-        Ok(os) => os,
-        Err(_) => {
-            "OS Release (/etc/os-release) could not be opened on client, insufficient rights?"
-                .into()
-        }
-    }
-}
-
-fn read_uptime() -> String {
-    match fs::read_to_string("/proc/uptime") {
-        Ok(uptime) => {
-            let (up_sec, _) = uptime.split_once(' ').unwrap_or(("0", "0"));
-            up_sec.into()
-        }
-        Err(_) => "Uptime (/proc/uptime) could not be read on client, insufficient rights?".into(),
-    }
 }
